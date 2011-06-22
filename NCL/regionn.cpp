0fa9b92ebfb2a04be69c92c6e8d9af73dd3db2d8
YoJajo <YoJajo@gmail.com>
2011-06-22 00:50:18 +0000

    licencing

--- NCL/regionn.cpp
+++ NCL/regionn.cpp
@@ -1,3 +1,25 @@
+/***************************************************************************
+ *   CreaTV Digital - Herramienta para la creación de aplicaciones NCL para
+ *   el middleware GINGA de Televisión Digital Terrestre
+ *
+ *  Copyright © 2010, 2011
+ *        -Cardozo, Sergio A. (yojajo@gmail.com)
+ *        -Schwartz, Sebastian M. (seba.schwartz.7@gmail.com)
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ****************************************************************************/
+
 /*!
 *\file NCL/regionn.cpp
 *\author Sergio A. Cardozo (yojajo@gmail.com)
